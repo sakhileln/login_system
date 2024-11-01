25f8d8ccfdba91d30fb5e6523c45f3aa696ac5a8
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2024-11-01 03:25:52 +0200

    Add boilerplate for login system.

---
+++ login.py
@@ -0,0 +1,9 @@
+print("______________________________")
+print("     A Simple Login System   ")
+print("******************************")
+print()
+username = input("Username: ")
+password = input("Password: ")
+print("______________________________")
+
+print(username, password)
