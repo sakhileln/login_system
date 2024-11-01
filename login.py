ff3f2c7c38d172fdbb551d69b3584f67f94beb6f
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2024-11-01 03:55:28 +0200

    Take user input for signing in or creating account.

--- login.py
+++ login.py
@@ -3,6 +3,16 @@ print("     A Simple Login System   ")
 print("******************************")
 print()
 
+print("1. Login")
+print("2. Create account")
+print("3. Exit")
+choice = input("Please choose 1, 2, or 3: ")
+while choice not in ["1", "2", "3"]:
+    print("")
+    print(f"Incorrect input: {choice}. Please try again...")
+    choice = input("Please choose 1, 2, or 3: ")
+choice = int(choice)
+
 
 def create_account() -> None:
     """
