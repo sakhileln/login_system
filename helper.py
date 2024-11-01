d2b2bbe268fdac422dd3ac9685e88e2531017173
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2024-11-01 04:04:23 +0200

    Add helper module for modularity.

---
+++ helper.py
@@ -0,0 +1,53 @@
+"""
+A module for helper functions.
+"""
+
+
+def create_account() -> None:
+    """
+    Function that handles user account creation
+
+    Parameters:
+        None.
+    Return:
+        None.
+    """
+    input_username = input("Username: ")
+    input_password = input("Password: ")
+    print("Please retype the passoword")
+    retype_password = input("Password: ")
+
+
+def sign_in() -> None:
+    """
+    Function that handles user sign in.
+
+    Parameters:
+        None.
+    Return:
+        None.
+    """
+    username = input("Username: ")
+    password = input("Password: ")
+    print("______________________________")
+
+
+def exit_program() -> None:
+    """
+    Exit the program.
+
+    Parameters:
+        None.
+    Return:
+        None.
+    """
+    print("Thank you for using our Space Software.")
+    print("See you soon...")
+    exit()
+
+
+if __name__ == "__main__":
+    # Test run
+    create_account()
+    sign_in()
+    exit_program()
