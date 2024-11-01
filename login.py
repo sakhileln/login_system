8303cbd5da1f5d6a3f974ba0c35c1a940ad41fd5
Sakhile L. Ndlazi <sandlazjhb024@student.wethinkcode.co.za>
2024-11-01 03:45:46 +0200

    Add create_account and sign_in functions.

--- login.py
+++ login.py
@@ -2,8 +2,35 @@ print("______________________________")
 print("     A Simple Login System   ")
 print("******************************")
 print()
-username = input("Username: ")
-password = input("Password: ")
-print("______________________________")
 
-print(username, password)
+
+def create_account() -> None:
+    """
+    Function that handles user account creation
+
+    Parameters:
+        None.
+    Return:
+        None.
+    """
+    input_username = input("Username: ")
+    input_password = input("Password: ")
+    print("Please retype the passoword")
+    retype_password = input("Password: ")
+
+
+def sign_in() -> None:
+    """
+    Function that handles user sign in.
+
+    Parameters:
+        None.
+    Return:
+        None.
+    """
+    username = input("Username: ")
+    password = input("Password: ")
+    print("______________________________")
+
+
+# print(username, password)
