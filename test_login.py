c8ec4122e5b057aad860d9b4e10261b5050083e7
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-01 05:22:00 +0200

    Test cases for short and long passwords.

--- test_login.py
+++ test_login.py
@@ -2,11 +2,26 @@
 Test cases for the login system module.
 """
 
+from password_validator import check_length
 import unittest
 
 
 class TestLoginSystem(unittest.TestCase):
-    pass
+    """
+    Test cases for modules
+    """
+
+    def test_short_password(self):
+        """
+        Test short password
+        """
+        self.assertFalse(check_length("short"))
+
+    def test_long_passowrd(self):
+        """
+        Test long password
+        """
+        self.assertTrue("longpassword")
 
 
 if __name__ == "__main__":
