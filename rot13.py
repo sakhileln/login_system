c0a9b17f7cb0581baf80501d52c599bbcb64d5fe
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-02 10:18:51 +0200

    Add encryption module for encryting passwords.

---
+++ rot13.py
@@ -0,0 +1,10 @@
+def encrypt(string: str) -> str:
+    """
+    Encrypt given string using ROT13 cipher.
+
+    Parameters:
+        string (str): The string to encrypt
+    Return:
+        string (str): Encrypted string
+    """
+    pass
