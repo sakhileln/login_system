b6aa7f68f6bdf995eda7fff69f722d87545c8534
Sakhile L. Ndlazi <sakhilelindah@gmail.com>
2024-11-02 15:20:41 +0200

    Add function that creates the users table.

--- database.py
+++ database.py
@@ -1,3 +1,10 @@
+"""
+A module for handling database read and write.
+"""
+
+import sqlite3
+
+
 def create_database() -> None:
     """
     The function will create the database tables once.
@@ -7,4 +14,16 @@ def create_database() -> None:
     Return:
         None
     """
-    pass
+    # Initiate connection to the database
+    connection = sqlite3.connect("credentials.sqlite")
+
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE users(username, password)")
+    res = cursor.execute("SELECT * FROM sqlite_master")
+
+    # Close the connection once done
+    connection.close()
+
+
+if __name__ == "__main__":
+    # create_database()
